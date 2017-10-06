9c9a3549bebe586a5a9dc61b4064b700ccb30c97
wisnu kurniawan <wisnuko13@gmail.com>
2017-10-06 08:36:09 +0700

    Initial commit

---
+++ src/main/kotlin/com/kurniawan/wisnu/belajarspringboot/BelajarSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.kurniawan.wisnu.belajarspringboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BelajarSpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BelajarSpringBootApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/kurniawan/wisnu/belajarspringboot/BelajarSpringBootApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kurniawan.wisnu.belajarspringboot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BelajarSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
